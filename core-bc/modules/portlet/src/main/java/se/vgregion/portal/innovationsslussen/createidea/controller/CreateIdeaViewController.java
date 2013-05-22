050365c4f3e7003709e643d6923027d1fb59bce2
mrg3kko@gmail.com <mrg3kko@gmail.com@bb39c091-d7d5-707a-2332-daf75bf8f7f8>
2013-05-22 11:36:09 +0000

    Added portlet skeleton for create idea portlet

---
+++ core-bc/modules/portlet/src/main/java/se/vgregion/portal/innovationsslussen/createidea/controller/CreateIdeaViewController.java
@@ -0,0 +1,61 @@
+package se.vgregion.portal.innovationsslussen.createidea.controller;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+
+/**
+ * Controller class for the view mode in the create idea portlet.
+ *
+ * @author Erik Andersson
+ * @company Monator Technologies AB
+ */
+@Controller
+@RequestMapping(value = "VIEW")
+public class CreateIdeaViewController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreateIdeaViewController.class.getName());
+
+    /**
+     * Constructor.
+     *
+     */
+    /*
+    @Autowired
+    public IdeaViewController() {
+    }
+    */
+
+    /**
+     * The default render method.
+     *
+     * @param request  the request
+     * @param response the response
+     * @param model    the model
+     * @return the view
+     */
+    @RenderMapping()
+    public String showIdea(RenderRequest request, RenderResponse response, final ModelMap model) {
+
+        ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+        long scopeGroupId = themeDisplay.getScopeGroupId();
+        long companyId = themeDisplay.getCompanyId();
+        boolean isSignedIn = themeDisplay.isSignedIn();
+
+        model.addAttribute("foo", "bar");
+
+        return "view";
+    }
+
+}
+
