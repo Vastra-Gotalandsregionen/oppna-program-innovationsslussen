32fac17d6c3734070961024d5001137c082c67c2
mrg3kko@gmail.com <mrg3kko@gmail.com@bb39c091-d7d5-707a-2332-daf75bf8f7f8>
2013-06-19 08:49:35 +0000

    Removed old .svn folder that was checked into resource-action in svc project by mistake. Added skeleton for user settings portlet.

---
+++ core-bc/modules/portlet/src/main/java/se/vgregion/portal/innovationsslussen/usersettings/controller/UserSettingsViewController.java
@@ -0,0 +1,100 @@
+package se.vgregion.portal.innovationsslussen.usersettings.controller;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import se.vgregion.portal.innovationsslussen.domain.IdeaConstants;
+import se.vgregion.portal.innovationsslussen.domain.jpa.Idea;
+import se.vgregion.portal.innovationsslussen.domain.jpa.IdeaContent;
+import se.vgregion.service.innovationsslussen.IdeaService;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.messageboards.model.MBMessageDisplay;
+import com.liferay.portlet.messageboards.model.MBThread;
+import com.liferay.portlet.messageboards.service.MBMessageLocalServiceUtil;
+
+/**
+ * Controller class for the view mode in idea portlet.
+ *
+ * @author Erik Andersson
+ * @company Monator Technologies AB
+ */
+@Controller
+@RequestMapping(value = "VIEW")
+public class UserSettingsViewController {
+
+	IdeaService ideaService;
+	
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserSettingsViewController.class.getName());
+
+    /**
+     * Constructor.
+     *
+     */
+    /*
+    @Autowired
+    public UserSettingsViewController(IdeaService ideaService) {
+        this.ideaService = ideaService;
+    }
+    */   
+
+    /**
+     * The default render method.
+     *
+     * @param request  the request
+     * @param response the response
+     * @param model    the model
+     * @return the view
+     */
+    @RenderMapping()
+    public String showIdea(RenderRequest request, RenderResponse response, final ModelMap model) {
+
+    	ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+        long scopeGroupId = themeDisplay.getScopeGroupId();
+        long companyId = themeDisplay.getCompanyId();
+        long userId = themeDisplay.getUserId();
+        
+        try {
+			User user = UserLocalServiceUtil.getUser(userId);
+			
+			String userFullName = user.getFullName();
+			
+			model.addAttribute("userFullName", userFullName);
+			
+		} catch (PortalException e) {
+			LOGGER.error(e.getMessage(), e);
+		} catch (SystemException e) {
+			LOGGER.error(e.getMessage(), e);
+		}
+        
+        
+        
+        return "view";
+    }
+    
+    
+
+}
+
--- core-bc/modules/portlet/src/main/webapp/WEB-INF/jsp/idea_list/view.jsp
+++ core-bc/modules/portlet/src/main/webapp/WEB-INF/jsp/idea_list/view.jsp
@@ -11,65 +11,64 @@
 <portlet:defineObjects />
 <liferay-theme:defineObjects />	
 
-<c:choose>
-	<c:when test="${ideaListType == '1'}">
-		<h2>Mina id&eacute;er</h2>
-	</c:when>
-	<c:when test="${ideaListType == '2'}">
-		<h2>Mina favoriter</h2>
-	</c:when>
-	<c:when test="${ideaListType == '3'}">
-		<h2>St&auml;ngda id&eacute;er</h2>
-	</c:when>
-	<c:otherwise></c:otherwise>
-</c:choose>
-
-<ul class="idea-list">
+<div class="idea-list-wrap">
+	<c:choose>
+		<c:when test="${ideaListType == '1'}">
+			<h2 class="title">Mina id&eacute;er</h2>
+		</c:when>
+		<c:when test="${ideaListType == '2'}">
+			<h2 class="title">Mina favoriter</h2>
+		</c:when>
+		<c:when test="${ideaListType == '3'}">
+			<h2 class="title">St&auml;ngda id&eacute;er</h2>
+		</c:when>
+		<c:otherwise></c:otherwise>
+	</c:choose>
 	
-	<c:forEach items="${ideaList}" var="idea" varStatus="index">
+	<ul class="idea-list">
+		
+		<c:forEach items="${ideaList}" var="idea" varStatus="index">
+		
+			<liferay-portlet:renderURL var="ideaUrl" plid="${ideaPlid}" portletName="${ideaPortletName}">
+				<liferay-portlet:param name="showView" value="showIdea" />
+				<liferay-portlet:param name="urlTitle" value="${idea.urlTitle}" />
+			</liferay-portlet:renderURL>
+		
+			<li>
+				<div class="idea">
+					<div class="idea-content">
 	
-		<liferay-portlet:renderURL var="ideaUrl" plid="${ideaPlid}" portletName="${ideaPortletName}">
-			<liferay-portlet:param name="showView" value="showIdea" />
-			<liferay-portlet:param name="urlTitle" value="${idea.urlTitle}" />
-		</liferay-portlet:renderURL>
-	
-		<li>
-			<div class="idea">
-				<div class="idea-content">
-
-					<h2 class="title">
-						<a href="${ideaUrl}">${idea.title}</a>
-					</h2>
-					
-					<div class="clearfix">
-						<ul class="idea-stats">
-							<li class="likes">
-							(${fn:length(idea.likes)})
-							</li>
-						</ul>
-						<div class="idea-info">
-							<div class="description">
-								<a href="${ideaUrl}">
-									<a href="${ideaUrl}">${fn:substring(idea.ideaContentPrivate.description, 0, 150)}</a>
-								</a>
+						<h2 class="title">
+							<a href="${ideaUrl}">${idea.title}</a>
+						</h2>
+						
+						<div class="clearfix">
+							<ul class="idea-stats">
+								<li class="likes">
+								(${fn:length(idea.likes)})
+								</li>
+							</ul>
+							<div class="idea-info">
+								<div class="description">
+									<a href="${ideaUrl}">
+										<a href="${ideaUrl}">${fn:substring(idea.ideaContentPrivate.description, 0, 150)}</a>
+									</a>
+								</div>
 							</div>
 						</div>
+						
+						<ul class="idea-flow-list">
+							<li class="done"><span>1</span></li>
+							<li class="done"><span>2</span></li>
+							<li><span>3</span></li>
+							<li><span>4</span></li>
+							<li><span>5</span></li>
+							<li><span>6</span></li>
+						</ul>
 					</div>
-					
-					<ul class="idea-flow-list">
-						<li class="done"><span>1</span></li>
-						<li class="done"><span>2</span></li>
-						<li><span>3</span></li>
-						<li><span>4</span></li>
-						<li><span>5</span></li>
-						<li><span>6</span></li>
-					</ul>
 				</div>
-			</div>
-		</li>
-	</c:forEach>	
-
-</ul>
-
-
-
+			</li>
+		</c:forEach>	
+	
+	</ul>
+</div>
