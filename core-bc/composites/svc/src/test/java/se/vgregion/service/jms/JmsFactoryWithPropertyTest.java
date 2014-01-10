d4f8b08e51bf580df7833bb70f6eb635d265e05d
patrik.bergstrom@knowit.se <patrik.bergstrom@knowit.se@bb39c091-d7d5-707a-2332-daf75bf8f7f8>
2014-01-10 09:20:10 +0000

    Added embedded Active MQ broker. Added logging on exception. Changed logging level to debug on several logging events.

--- core-bc/composites/svc/src/test/java/se/vgregion/service/jms/JmsFactoryWithPropertyTest.java
+++ core-bc/composites/svc/src/test/java/se/vgregion/service/jms/JmsFactoryWithPropertyTest.java
@@ -24,6 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -36,6 +37,7 @@ import static org.junit.Assert.assertNotNull;
 @ContextConfiguration({"classpath:messaging-context.xml", "classpath:mock-context.xml",
         "classpath:test-jpa-infrastructure-configuration.xml", "classpath:broker-url-property.xml",
         "classpath:applicationContext-test.xml"})
+@DirtiesContext
 public class JmsFactoryWithPropertyTest {
 
     @Autowired
