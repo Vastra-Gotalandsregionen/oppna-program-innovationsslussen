e80803621fcf1dbff929fbe0b30a46186040a7cb
Patrik Björk <patrik.bjork@codespirit.se>
2018-04-27 14:36:46 +0200

    Sort idea transporters in select.

--- core-bc/composites/svc/src/main/java/se/vgregion/service/search/DbSearchServiceImpl.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/service/search/DbSearchServiceImpl.java
@@ -1,5 +1,6 @@
 package se.vgregion.service.search;
 
+import org.apache.commons.collections.bag.TreeBag;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import se.vgregion.portal.innovationsslussen.domain.jpa.Idea;
 import se.vgregion.service.innovationsslussen.repository.idea.IdeaRepository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -43,8 +45,10 @@ public class DbSearchServiceImpl implements SearchService {
             }
         }
 
+        Map.Entry<String, Long>[] entryArray = getEntriesSortedByValues(collect);
+
         FacetField facetField = new FacetField("privateIdeaTransporter");
-        for (Map.Entry<String, Long> stringLongEntry : collect.entrySet()) {
+        for (Map.Entry<String, Long> stringLongEntry : entryArray) {
             facetField.add(stringLongEntry.getKey(), stringLongEntry.getValue());
         }
 
@@ -59,6 +63,28 @@ public class DbSearchServiceImpl implements SearchService {
         // "totalIdeasCount" mapped to Long
         // "ideas" mapped to List<Idea>
         return returnMap;
+    }
+
+    private Map.Entry<String, Long>[] getEntriesSortedByValues(Map<String, Long> collect) {
+        Map.Entry<String, Long>[] entryArray = new Map.Entry[collect.size()];
+        ArrayList<Long> theNumbersOrdered = new ArrayList<>(new TreeBag(collect.values()));
+
+        for (Map.Entry<String, Long> stringLongEntry : collect.entrySet()) {
+            int index = collect.size() - 1 - theNumbersOrdered.indexOf(stringLongEntry.getValue());
+            while (true) {
+                if (entryArray[index] == null) {
+                    entryArray[index] = stringLongEntry;
+                    if (!theNumbersOrdered.get(collect.size() - 1 - index).equals(stringLongEntry.getValue())) {
+                        throw new RuntimeException("The value at index=" + index + " should be "
+                                + stringLongEntry.getValue());
+                    }
+                    break;
+                } else {
+                    index--;
+                }
+            }
+        }
+        return entryArray;
     }
 
 }
